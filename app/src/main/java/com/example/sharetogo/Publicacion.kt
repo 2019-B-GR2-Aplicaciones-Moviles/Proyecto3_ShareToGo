30ab77c49639200383ac2bee542454c6753bdf76
AngelDYaAg <angel_0930@hotmail.es>
2019-12-04 12:34:41 -0500

    ajustes pantalla

--- app/src/main/java/com/example/sharetogo/Publicacion.kt
+++ app/src/main/java/com/example/sharetogo/Publicacion.kt
@@ -1,12 +1,19 @@
 package com.example.sharetogo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Publicacion : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_publicacion)
+    }
+
+    fun onClickButtonPublicacion(view: View) {
+        var intent = Intent(this, pantallaPrincipal::class.java)
+        startActivity(intent)
     }
 }
