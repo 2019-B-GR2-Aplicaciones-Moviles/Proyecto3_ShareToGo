abf9836a50f47d1ac8a7d5947add75d3ce9c6aa0
AngelDYaAg <angel_0930@hotmail.es>
2019-12-03 23:19:50 -0500

    activities

---
+++ app/src/main/java/com/example/sharetogo/seleccionSectores.kt
@@ -0,0 +1,12 @@
+package com.example.sharetogo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class seleccionSectores : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleccion_sectores)
+    }
+}
