7eac7b98b60a9cdd4fffccb371a19e15152750f4
JHyel2015 <elgeorh@gmail.com>
2019-12-04 01:39:36 -0500

    Conexion entre activities

--- app/src/main/java/com/example/sharetogo/pantallaPrincipal.kt
+++ app/src/main/java/com/example/sharetogo/pantallaPrincipal.kt
@@ -1,12 +1,24 @@
 package com.example.sharetogo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class pantallaPrincipal : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_principal)
+    }
+
+    fun onClickButtonUsar ( view: View ) {
+        var intent = Intent( this, seleccionSectores::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickButtonCompartir ( view: View ) {
+        var intent = Intent(this, SeleccionSectoresPublicar::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/sharetogo/seleccionSectores.kt
+++ app/src/main/java/com/example/sharetogo/seleccionSectores.kt
@@ -1,12 +1,19 @@
 package com.example.sharetogo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class seleccionSectores : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seleccion_sectores)
+    }
+
+    fun onClickButtonSectorContinuar(view: View) {
+        var intent = Intent(this, SeleccionRuta::class.java)
+        startActivity(intent)
     }
 }
