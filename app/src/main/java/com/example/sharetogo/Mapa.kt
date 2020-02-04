49dcf024a494b7530c422b455e4de37395bab4e6
AngelDYaAg <angel_0930@hotmail.es>
2020-02-04 11:56:45 -0500

    permisos mapa

--- app/src/main/java/com/example/sharetogo/Mapa.kt
+++ app/src/main/java/com/example/sharetogo/Mapa.kt
@@ -1,5 +1,7 @@
 package com.example.sharetogo
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.maps.GoogleMap
@@ -7,6 +9,8 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import android.widget.Toast
 import android.location.Location
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class Mapa : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
@@ -35,14 +39,23 @@ class Mapa : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButt
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
+        } else {
+            Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),0)
+        }
 
         // Add a marker in Sydney and move the camera
         //val sydney = LatLng(-34.0, 151.0)
         //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        mMap.setMyLocationEnabled(true)
-        mMap.setOnMyLocationButtonClickListener(this);
-        mMap.setOnMyLocationClickListener(this);
+        //mMap.setMyLocationEnabled(true)
+
     }
 
     override fun onMyLocationClick(location: Location) {
@@ -50,11 +63,11 @@ class Mapa : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButt
     }
 
     override fun onMyLocationButtonClick(): Boolean {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show()
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
     }
 
-    
+
 }
