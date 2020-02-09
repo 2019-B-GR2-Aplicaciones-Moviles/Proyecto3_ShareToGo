aaf6201282e2dd98977e943b5995f010b0bd5488
AngelDYaAg <angel_0930@hotmail.es>
2020-02-08 20:16:03 -0500

    interfaz compartir

---
+++ app/src/main/java/com/example/sharetogo/MenuRegistroSentido.kt
@@ -0,0 +1,12 @@
+package com.example.sharetogo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuRegistroSentido : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_registro_sentido)
+    }
+}
