373aa6f27188a14365dcaa3afe15aab26fb556f3
Mishelle-Quinchiguango <mishelle.quinchiguango@epn.edu.ec>
2020-02-08 19:13:35 -0500

    ListView

--- app/src/main/java/com/example/sharetogo/Publicacion.kt
+++ app/src/main/java/com/example/sharetogo/Publicacion.kt
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_publicacion.*
+import com.facebook.appevents.codeless.internal.ViewHierarchy.setOnClickListener
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.widget.*
+import java.util.ArrayList
 
 
 class Publicacion : AppCompatActivity() {
@@ -19,17 +25,27 @@ class Publicacion : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun anadirCalles(view: View){
-        val linkedHashMap = LinkedHashMap<Int, String>()
-        val valor= editTextCalles.text.toString()
-        linkedHashMap.put(1, valor)
+    lateinit var list: ListView
+    lateinit var items: ArrayList<String>
 
-// Imprimimos el Map con un Iterador que ya hemos instanciado anteriormente
-       val it = linkedHashMap.keys.iterator()
-        while (it.hasNext()) {
-            val key = it.next()
-            println("Clave: " + key + " -> Valor: " + linkedHashMap[key])
-            
-        }
+    fun anadir(view: View)
+    {
+        val txtEditText = findViewById<EditText>(R.id.editTextCalles)
+        val btnAnadirCalles = findViewById<Button>(R.id.btnAnadirCalles)
+        list = findViewById(R.id.list)
+        items=ArrayList<String>()
+        val adp = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        list.adapter=adp
+
+        btnAnadirCalles.setOnClickListener(View.OnClickListener {
+            val nombreCalles = editTextCalles.getText().toString()
+            if (nombreCalles.isEmpty()) {
+                Toast.makeText(this, "No ha ingresado ningun nombre de calle", Toast.LENGTH_SHORT).show()
+            }else{
+                items.add(txtEditText.text.toString())
+            }
+        })
+
+
     }
 }
