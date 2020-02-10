d92c2cebb865b82f4e9ff5eb3ebae29baaaf4545
Mishelle-Quinchiguango <mishelle.quinchiguango@epn.edu.ec>
2020-02-09 20:09:51 -0500

    FuncionEliminarrCalle

--- app/src/main/java/com/example/sharetogo/Publicacion.kt
+++ app/src/main/java/com/example/sharetogo/Publicacion.kt
@@ -1,37 +1,28 @@
 package com.example.sharetogo
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_publicacion.*
-import com.facebook.appevents.codeless.internal.ViewHierarchy.setOnClickListener
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.widget.*
 import java.util.ArrayList
 
 
 class Publicacion : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_publicacion)
-    }
-
-    fun onClickButtonPublicacion(view: View) {
-        var intent = Intent(this, pantallaPrincipal::class.java)
-        startActivity(intent)
-    }
-
     lateinit var items: ArrayList<String>
     lateinit var  adp: ArrayAdapter<String>
     lateinit var list: ListView
     lateinit var  et1 :EditText
+    var contId:Int=0
 
-    fun onClickButtonAnadirCalles(view: View)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publicacion)
+
 
         items=ArrayList<String>()
         adp = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
@@ -40,21 +31,50 @@ class Publicacion : AppCompatActivity() {
 
         et1 = findViewById<EditText>(R.id.editTextCalles)
 
+        elimnarCalles()
+
+    }
+
+    fun onClickButtonPublicacion(view: View) {
+        var intent = Intent(this, pantallaPrincipal::class.java)
+        startActivity(intent)
+
+    }
+
+
+
+    fun onClickButtonAnadirCalles(view: View)
+    {
         btnAnadirCalles.setOnClickListener(View.OnClickListener {
             val nombreCalles = et1.text.toString()
             if (nombreCalles.isEmpty()) {
                 Toast.makeText(this, "No ha ingresado ningun nombre de calle", Toast.LENGTH_SHORT).show()
             }else{
-                items.add(et1.text.toString())
+                contId++
+                items.add(contId.toString() +"     "+ et1.text.toString())
                 adp.notifyDataSetChanged()
                 et1.setText("")
+                Toast.makeText(this, "id   "+ contId + "       nombre    " + nombreCalles, Toast.LENGTH_SHORT).show()
             }
         })
     }
 
-    fun consultar(){
+    fun elimnarCalles(){
 
-
-
+        list.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_delete)
+                    .setTitle("Importante")
+                    .setMessage("¿Esta seguro de eliminar este nombre de calle?")
+                    .setPositiveButton(
+                        "Confirmar"
+                    ) { dialog, which ->
+                        items.removeAt(position)
+                        adp.notifyDataSetChanged()
+                        contId--
+                    }
+                    .setNegativeButton("Cancelar", null).show()
+            }
     }
 }
