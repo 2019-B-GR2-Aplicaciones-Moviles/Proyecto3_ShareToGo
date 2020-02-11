2c393df0afc264861339c02797bc204c9c833a6a
Theo96sc <theo_96.sc@outlook.es>
2020-02-11 18:04:16 -0500

    Seleccion Sectores Listo

--- app/src/main/java/com/example/sharetogo/lista_sectores_disponibles.kt
+++ app/src/main/java/com/example/sharetogo/lista_sectores_disponibles.kt
@@ -2,27 +2,51 @@ package com.example.sharetogo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ListView
 import android.widget.ArrayAdapter
-
+import android.widget.RadioButton
+import android.widget.Toast
+import java.util.ArrayList
 
 
 class lista_sectores_disponibles : AppCompatActivity() {
 
-    private val lstView: ListView? = null
+    lateinit var items: java.util.ArrayList<String>
+    lateinit var  adp: ArrayAdapter<String>
+    lateinit var list: ListView
+
+    private var sentido:String?=""
+    private var ERROR:String?="ERROR"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_sectores_disponibles)
 
+        val bundle :Bundle?=intent.extras
 
-        val listViewItems = ArrayList<String>()     //Aqui vienen los datos de la base..?
+        if (bundle!=null){
+            sentido = bundle.getString("sentidoBD")
+            Toast.makeText(this, sentido, Toast.LENGTH_SHORT).show()
+        }
 
-        val adapter: ArrayAdapter<String>
+        else{
+            Toast.makeText(this, ERROR, Toast.LENGTH_SHORT).show()
+        }
 
-        val lstView = findViewById<ListView>(R.id.listaSectores_View);
+
+
+        items= ArrayList<String>()
+        adp = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        list = findViewById(R.id.listaSectores_View)
+        list.adapter=adp
+
+//        sentidoLlegada = bundle?.getString("sentidoLlegada")
+//        sentidoSalida = bundle?.getString("sentidoSalida")
 
     }
+
+
 
 
 
